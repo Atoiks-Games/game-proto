c3f8fcfb8ec33778d082c9dab4b6437d8bfe9c69
Plankp T <plankp@outlook.com>
2017-03-11 09:08:55 -0500

    Fix awkward cut-off

--- src/main/java/com/atoiks/proto/GFrame.java
+++ src/main/java/com/atoiks/proto/GFrame.java
@@ -53,7 +53,7 @@ public class GFrame extends JFrame {
 
     public GFrame (String title, GScene... scenes) {
 	super (title);
-	setSize (WIDTH, HEIGHT);
+	setSize (WIDTH, HEIGHT + 30);
 	setResizable (false);
 	setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
 
