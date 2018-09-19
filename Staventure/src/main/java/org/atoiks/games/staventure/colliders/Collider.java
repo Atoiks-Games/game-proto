47ffe8d7ef0c63bfad13bc8155f858f3312c9496
Paul Teng <plankp@outlook.com>
2018-09-19 13:17:21 -0400

    Add utility methods to collider

--- Staventure/src/main/java/org/atoiks/games/staventure/colliders/Collider.java
+++ Staventure/src/main/java/org/atoiks/games/staventure/colliders/Collider.java
@@ -7,4 +7,18 @@ public interface Collider {
     public void render(IGraphics g);
 
     public boolean collidesWith(Collider collider);
+
+    public default boolean collidesWithAny(final Collider... colliders) {
+        for (final Collider col : colliders) {
+            if (collidesWith(col)) return true;
+        }
+        return false;
+    }
+
+    public default boolean collidesWithAll(final Collider... colliders) {
+        for (final Collider col : colliders) {
+            if (!collidesWith(col)) return false;
+        }
+        return true;
+    }
 }
